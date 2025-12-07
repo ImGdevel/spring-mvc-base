b5e12bec190b4740d843407fe7327783e21138ce
SH Woo <imdlsrks.mc@gmail.com>
2025-12-07 14:55:58 +0900

    ♻️ refactor: 미사용 redis캐시 제거

--- app-api/src/main/java/com/spring/mvc/base/infra/redis/repository/RedisExampleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spring.mvc.base.infra.redis.repository;
-
-import com.spring.mvc.base.infra.redis.domain.RedisExampleEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RedisExampleRepository extends CrudRepository<RedisExampleEntity, String> {
-
-}
