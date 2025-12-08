50da98a0fd6e1e1c64167bcd8d23aa43cf8e3ccb
SH Woo <imdlsrks.mc@gmail.com>
2025-12-08 12:42:13 +0900

    🐛 fix: 캐시 스토리지를 사용하도록 수정

--- app-api/src/test/java/com/spring/mvc/base/config/annotation/IntegrationTest.java
+++ app-api/src/test/java/com/spring/mvc/base/config/annotation/IntegrationTest.java
@@ -2,7 +2,9 @@ package com.spring.mvc.base.config.annotation;
 
 import com.spring.mvc.base.config.ImageStorageMockConfig;
 import com.spring.mvc.base.config.JpaAuditingTestConfig;
+import com.spring.mvc.base.config.RedisMockConfig;
 import com.spring.mvc.base.config.TestSecurityConfig;
+import com.spring.mvc.base.infra.redis.config.RedisConfig;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -17,13 +19,18 @@ import org.springframework.test.context.ActiveProfiles;
  * 통합 테스트 공통 설정을 모아둔 메타 어노테이션입니다.
  * - @ActiveProfiles(\"test\") : 테스트 프로필 활성화
  * - @SpringBootTest + @AutoConfigureMockMvc : 전체 컨텍스트 + MockMvc 구동
- * - @Import(TestSecurityConfig, JpaAuditingTestConfig, ImageStorageMockConfig) :
- *   보안, JPA Auditing, 이미지 스토리지에 대한 테스트 전용 설정 주입
+ * - @Import(TestSecurityConfig, JpaAuditingTestConfig, ImageStorageMockConfig, RedisMockConfig) :
+ *   보안, JPA Auditing, 이미지 스토리지, 캐시 스토리지에 대한 테스트 전용 설정 주입
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ActiveProfiles("test")
-@Import({TestSecurityConfig.class, JpaAuditingTestConfig.class, ImageStorageMockConfig.class})
+@Import({
+        TestSecurityConfig.class,
+        JpaAuditingTestConfig.class,
+        ImageStorageMockConfig.class,
+        RedisMockConfig.class
+})
 @SpringBootTest
 @AutoConfigureMockMvc
 @Tag("integration")
